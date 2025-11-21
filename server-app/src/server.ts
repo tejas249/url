c87f74f896ba33a9aa48b9a00779c168bd8af516
Tejas Kamble <tejaskambletk132@gmail.com>
2025-11-21 11:34:39 +0530

    first commit

---
+++ server-app/src/server.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+
+
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+
+
+app.listen(port, ()=>{
+    console.log("Sever started successfully on PORT : ${port}")
+})
